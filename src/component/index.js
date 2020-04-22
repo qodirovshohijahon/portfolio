3ae8b8cc1a8fac71f9b20e11e1ca51fde6e2113d
qodirovshohijahon <tasaddux@gmail.com>
2020-04-22 10:05:31 +0500

    Navbar comp createt

---
+++ src/component/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Navbar from "./Navbar";
+const Home = () => {
+    return (
+        <div>
+        <Navbar />
+        </div>
+    )
+}
+
+export default Home;
